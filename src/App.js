f6ad59c47f53a9bce8c394239b6dd9a9bab06edf
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-06 10:47:17 +1100

    started creating a toolbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Toolbar from './components/Toolbar/Toolbar'
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
+      <Toolbar/>
       <h1>hello</h1>
     </React.Fragment>
   );
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const toolbar = props => (
+  <header>
+    <nav>
+      <div><a href="/">letter-jumble</a></div>
+      <div>
+        <ul>
+          <li><button>jumble</button></li>
+          <li><a href="/"></a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default toolbar;
