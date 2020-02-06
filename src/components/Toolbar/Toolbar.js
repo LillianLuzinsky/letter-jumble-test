742eb52f9598aaaf0073256594dfaccd11daa12b
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-06 11:48:36 +1100

    added static hamburger menu button

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import DrawerToggleButton from '../SideDraw/DrawerToggleButton'
 import './Toolbar.css';
 
 const toolbar = props => (
   <header className="toolbar">
     <nav className="toolbar__navigation">
-      <div></div>
+      <div>
+        <DrawerToggleButton/>
+      </div>
       <div className="toolbar__logo">
         <a href="/">letter jumble</a>
       </div>
