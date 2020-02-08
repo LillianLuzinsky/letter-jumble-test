412ea3fe307733c4bafc3af744b0fa42f2bb2dec
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-08 20:48:48 +1100

    trying to add svg into alphabet array

--- src/components/Alphabet/Letter.js
+++ src/components/Alphabet/Letter.js
@@ -12,12 +12,12 @@ export const LetterContainer = styled.span`
   display: inline-block;
   width: 12vw;
   height: 12vw;
-  transition: 2s;
-  animation: colourRange 4s linear infinite;
+  /* transition: 2s; */
+  /* animation: colourRange 4s linear infinite; */
 
   :hover {
     background: white;
-    transition: 0s;
+    transition: 3s;
     /* animation: colourRange 2s linear infinite; */
 
     @keyframes colourRange{
